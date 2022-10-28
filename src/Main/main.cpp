86a143d3e181b900fd884dbb1a2bc52634534a55
theseung <pcappli@koreatech.ac.kr>
2022-10-28 02:43:25 +0000

    Fix:window name

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     QApplication a(argc, argv);
-    a.setApplicationName("QHAC3");
+    a.setApplicationName("SUV GCS");
     a.setQuitOnLastWindowClosed(true);
 
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
